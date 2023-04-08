b4eb7a77c97b99e093af734ba9014b174fcd6e85
FahadIshaq <fa20bcs017@gmail.com>
2023-04-08 22:28:58 +0500

    First commit

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,117 +1,100 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, StatusBar, ScrollView } from "react-native";
-import { Button, TextInput } from "react-native-paper";
+import { Text, View, ScrollView, StyleSheet, StatusBar, TouchableOpacity } from "react-native";
+import { TextInput, Button } from 'react-native-paper';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 
-export function Main() {
-    const [item, setItem] = useState("");
-const [list, setList] = useState([]);
-const [edit, setEdit] = useState(null);
-const addItem = () => {
-    setList([...list, { id: list.length, value: item }]);
-    setItem('');  
-};
+const App = () => {
+  const [item, setItem] = useState("");
+  const [edit, setEdit] = useState(0);
+  const [todos, setTodos] = useState([]);
 
-const deleteItem = (id) => {
-    setList(list.filter(item => item.id !== id));
-};
+  const handleAdd = () => {
+    if (item && !edit) {
+      setTodos([...todos, {id: Date.now().toString(), text: item}]);
+      setItem("");
+    } else if (item && edit) {
+      const newTodos = todos.map(todo => {
+        if (todo.id === edit) {
+          return {...todo, text: item};
+        }
+        return todo;
+      });
+      setTodos(newTodos);
+      setItem("");
+      setEdit(0);
+    }
+  };
 
-const updateItem = (id, newItem) => {
+  const handleEdit = (id, text) => {
     setEdit(id);
-    setItem(newItem);
-};
+    setItem(text);
+  };
 
-const handleUpdate = () => {
-    const newList = [...list];
-    const index = newList.findIndex(item => item.id === edit);
-    newList[index].value = item;
-    setList(newList);
-    setItem('');
-    setEdit(null);
-};
+  const handleDelete = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
 
-return (
-
-    <View style={{ backgroundColor: "white", flex: 1 }}>
-        <StatusBar
-            backgroundColor="thistle"
-        />
-        <Text style={mystyle.header}>To do List</Text>
-        <View style={{ justifyContent: "center", alignItems: "center", flexDirection: "row", alignSelf: "center", width: "80%", marginTop: 20, marginBottom : 20}}>
-            <TextInput
-                mode='outlined'
-                style={mystyle.input}
-                label="Enter an item"
-                outlineColor="thistle"
-                activeOutlineColor="darkgray"
-                textColor="gray"
-                value={item}
-                onChangeText={text => setItem(text)}
-                right={<TextInput.Icon style={{ paddingTop: 5 }} icon="arrow-down-drop-circle-outline" />}
-            >
-            </TextInput>
-            <View style={{ marginLeft: 15 }}>
-                <Button mode='contained' buttonColor={item ? "thistle" : "lightgray"} style={{ paddingLeft: 5, paddingRight: 5 }} onPress={edit != null ? handleUpdate : addItem}>
-                    <Text style={{ fontSize: 15 }}> {edit != null ? 'Update Item' : 'Add Item'}</Text>
-                </Button>
-            </View>
-
-        </View>
-
-
-        <ScrollView>
-            {list.map(item => (
-                <View key={item.id} style={mystyle.itemsList}>
-                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                        <Text style={mystyle.itemNo}>{item.id + 1}</Text>
-                        <TouchableOpacity key={item.id} onPress={() => updateItem(item.id, item.value)}>
-                            <Text key={item.id} style={{ color: 'grey', fontSize: 16,maxWidth:230, fontStyle:"italic"}}>
-{item.value}
-</Text>
-</TouchableOpacity>
-</View>
-<TouchableOpacity onPress={() => deleteItem(item.id)}>
-<MaterialIcons name="delete" size={25} color="thistle" style={{ padding:10}}/>
-</TouchableOpacity>
-</View>
-))}</ScrollView>
+  return (
+    <View style={{flex: 1}}>
+      <StatusBar backgroundColor="lightblue" />
+      <Text style={mystyleSheet.header}>To do List</Text>
+      <View style={mystyleSheet.inputTxt}>
+      <TextInput
+        mode="outlined"
+        onChangeText={setItem}
+        value={item}
+        right={edit ? <TextInput.Icon icon="arrow-up-drop-circle-outline" /> : <TextInput.Icon icon="arrow-down-drop-circle-outline" />}
+        label="Enter an Item"
+        style={mystyleSheet.textField}
+      />
+      <View style={{justifyContent:"center", alignContent:"center", alignItems:"center"}}>
+      <Button style={{padding:2,marginLeft:12,  backgroundColor: item ? "lightblue" : "lightgray"}} mode="contained" onPress={handleAdd}>{edit ? "Update" : "Add"}</Button>
+      </View>
+      </View>
+      <View style={{marginTop: 20, width: "100%"}}>
+      <ScrollView>
+  {todos.map((todo, index) => (
+    <View key={todo.id} style={{ alignSelf:"center", flexDirection: "row", justifyContent: "space-between", alignItems: "center", width: "90%", padding: 10, borderColor: "lightgray", borderWidth: 2, marginTop: 8, borderRadius: 10 }}>
+      <TouchableOpacity style={{ flexDirection: "row", alignItems: "center" }}>
+        <Text style={mystyleSheet.setIndex}>{index + 1}</Text>
+        <Text style={mystyleSheet.handleEdit} onPress={() => handleEdit(todo.id, todo.text)}>{todo.text}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => handleDelete(todo.id)}>
+        <MaterialIcons name="delete" size={25} color="lightblue" />
+      </TouchableOpacity>
     </View>
+  ))}
+</ScrollView>
 
-)
+</View>
+</View>
+);
+};
 
-}
-
-const mystyle = StyleSheet.create({ header: {
-textAlign: "center",
+const mystyleSheet = StyleSheet.create({
+header: {
 color: "white",
-fontSize: 30,
-fontWeight: "bold",
-padding: 25,
-backgroundColor: "thistle"
-},
-input: {
-    height: 40,
-    width: "60%"
+fontSize: 40,
+backgroundColor: "lightblue",
+textAlign: "center",
+padding: 20,
+fontWeight: "bold"
 },
 
-itemsList: {
-    borderWidth: 0.7,
-    borderColor: "gray",
-    flexDirection: 'row',
-    alignItems: 'center',
-    alignSelf:"center",
-    borderRadius: 5,
-    justifyContent: 'space-between',
-    width: "80%",
-    padding:5,
-    marginBottom:20, },
+textField: {
+width: "60%"
+},
 
-itemNo: {
-    color: 'white',
-    padding: 8,
-    marginLeft: 10,
-    marginRight: 10,
-    backgroundColor: 'thistle'
-}
-}
-)
+inputTxt:{
+    justifyContent: "center", flexDirection: "row", width: "80%",
+    alignSelf:'center', 
+    marginTop:20
+}, 
+setIndex:{ fontSize: 15 ,padding:7, backgroundColor:"lightblue", color:"white"},
+handleEdit:{ fontSize: 20, marginLeft: 10, maxWidth: "80%", width:"80%", alignSelf:"center" }
+
+
+
+});
+
+export default App;
